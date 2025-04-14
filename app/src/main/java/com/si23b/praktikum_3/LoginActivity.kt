eca99a712edc0b61327045eb6252856bf92d75ea
wawiwuwawu <dedeari2005@gmail.com>
2025-04-14 07:18:27 +0700

    Merubah teks lagi

--- app/src/main/java/com/si23b/praktikum_3/LoginActivity.kt
+++ app/src/main/java/com/si23b/praktikum_3/LoginActivity.kt
@@ -42,7 +42,7 @@ class LoginActivity : AppCompatActivity() {
                 startActivity(intent)
                 finish()
             } else {
-                Toast.makeText(this, "Login Gagal, Perikasa kembali Password dan Email!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Login Gagal, Perikasa Password dan Email kembali!", Toast.LENGTH_SHORT).show()
             }
         }
         registerButton.setOnClickListener {
